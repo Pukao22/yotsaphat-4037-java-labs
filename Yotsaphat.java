7cef7d38847c5ec8598b328fe341a3311a2afe40
Pukao22 <yotsaphat.p@kkumail.com>
2024-11-18 16:14:17 +0700

    lab 1

--- Yotsaphat.java
+++
@@ -1,5 +0,0 @@
-public class Yotsaphat {
-    public static void main(String[] arge) {
-        System.out.println(x: "Hello World");
-    }
-}
---
+++ lab1/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] arge) {
+        System.out.println( "Hello my name is Yotsaphat Prasartsri and student ID is 673040403-7");
+    }
+}
